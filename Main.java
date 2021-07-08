3e98058547770db89cc70a7aa80ebb17ef2c716f
Alexander Mylnikov <53084160+amylnikov30@users.noreply.github.com>
2021-07-07 22:49:05 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+
+class Main {
+    public static void main(String[] args){
+      Runtime CPU = new Runtime();
+      CPU.run("TuringVM/FinalTuring/TuringVM.bin");
+      // CPU.run("doubles.bin");
+    }
+} 
